d44f6b98fc43cb21bfbe91465170367d6e953097
jiazhen <fdf515@gmail.com>
2015-09-30 21:23:24 +0100

    CMS logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :load_categories
+  before_action :load_page_contents
 
   def load_categories
     @categories = Category.where(:parent_id => nil).all
+  end
+
+  def load_page_contents
+    page_contents = PageContent.where("name IN (?)", ["logo"]).all
+    @logo = page_contents[0]
   end
 end
