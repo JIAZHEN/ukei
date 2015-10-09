8f93d55042ac7b917259df2c2f24d7a2f2b2268f
Joe Xie <fdf515@gmail.com>
2015-10-09 13:54:43 +0100

    Page content should be plain txt

--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -1,6 +1,4 @@
 class PageContent < ActiveRecord::Base
-  include Bootsy::Container
-
   has_attached_file :image, styles: { medium: "200x100" }, default_url: "missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
